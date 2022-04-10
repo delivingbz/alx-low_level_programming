c74705c1e0486d54fa8f22f42b2732af89cc11c9
kingsley <kingscode2@gmail.com>
2022-04-10 14:32:38 -0700

    numspace

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,11 @@
-/*
- * File: 9-print_comb.c
- * Auth: kingsley
- */
 #include <stdio.h>
+
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int num;
