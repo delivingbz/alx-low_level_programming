6dd5b3fd41c6bebcde9dfcf347d9c7744d52e9ab
kingsley <kingscode2@gmail.com>
2022-04-14 01:21:55 -0700

    c_debugging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,22 @@
+/*
+ * Filename: 0-main.c
+ * Auth: kingsley
+ *
+ */
+
+#include "main.h"
+/**
+ * main - main
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
+}
