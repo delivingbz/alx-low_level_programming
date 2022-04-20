b5a82c46af681a1e480f51de806b1250d9fe2b35
kingsley <kingscode2@gmail.com>
2022-04-20 13:24:53 -0700

    print-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+/*
+* File: 4-print_rev.c
+* Auth: kingsley
+*/
+
+#include "main.h"
+
+/**
+* print_rev - Prints a string in reverse.
+* @s: The string to be printed.
+*/
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	hile (s[index++])
+	len++;
+
+	for (index = len - 1; index >= 0; index--)
+						_putchar(s[index]);
+	_putchar('\n');
+}
