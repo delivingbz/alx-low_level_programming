5c11a73362dfdd1da598887793199888269989d2
kingsley <kingscode2@gmail.com>
2022-04-27 15:15:22 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,8 @@
 * @n: bytes of the memory
 * Return: dest
 */
-char  *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 	unsigned int i;
 	i = 0;
