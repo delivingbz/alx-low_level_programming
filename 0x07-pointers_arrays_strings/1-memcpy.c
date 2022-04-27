c5439786f4ea6e2e5635a3406c39ab0d3f49d272
kingsley <kingscode2@gmail.com>
2022-04-27 14:54:50 -0700

    memory add

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
 * @n: bytes of the memory
 * Return: dest
 */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char  *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	i = 0;
