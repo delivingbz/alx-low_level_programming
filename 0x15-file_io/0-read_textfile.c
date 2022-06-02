fbf67e019b216c4154d7996a8ce1f26d2f4b233a
kingsley <kingscode2@gmail.com>
2022-06-02 16:45:59 -0700

    file-io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
- */
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: pointer to text in a file
+* @letters: number of letters
+* Return: the actual number of letters it could read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     ssize_t file, fread, fwrite;
