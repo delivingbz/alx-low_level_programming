aa8fcde37ae50062e7a860377655ed3be0d5d58a
kingsley <kingscode2@gmail.com>
2022-06-02 17:08:29 -0700

    file-io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,47 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
-* read_textfile - reads a text file and prints it to the POSIX standard output
-* @filename: pointer to text in a file
-* @letters: number of letters
-* Return: the actual number of letters it could read and print
+* read_textfile - reads a text file and prints it to the POSIX stdout
+* @filename: file to read
+* @letters: number of letters it should read and print
+* Return: number of letters it could read and print or 0 if fails
 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	char *buff;
+	int of;
+	size_t lRead, check;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	if (!filename || !letters)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	buff = malloc(letters);
+	if (!buff)
+		return (0);
+	/* open file */
+	of = open(filename, O_RDONLY);
+	if (of == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	/* read file up to "letters" amount of charachters */
+	lRead = read(of, buff, letters);
+	if (lRead < 1)
+	{
+		free(buff);
+		close(of);
+		return (0);
+	}
+	/* write content read */
+	check = write(STDOUT_FILENO, buff, lRead);
+
+	free(buff);
+	close(of);
+
+	if (!check || check != lRead)
+		return (0);
+
+	return (lRead);
 }
